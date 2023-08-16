4117f319b088b5762b1556feeeea60a47fffc49d
Sinjoro Zilioni <102747842+sinjoro-zilioni@users.noreply.github.com>
2023-08-16 16:46:53 +0100

    Add files via upload

---
+++ belladonna_V1.4/keys.py
@@ -0,0 +1,12 @@
+
+# @username_bot -  key 02:
+# Your bot's API key goes below!
+clavis_ii = ""
+
+#Primary admin gold key.
+au_key = []
+
+#log channel for errors.
+LOG_CHANNEL = -000000000
+
+locked_out_reply = '🔒 Only key_bearers may use this function!!'
